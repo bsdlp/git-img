59f86c2e0a994d9d68363eaf7fa86cc534406f11
Jon Chen <bsd@voltaire.sh>
2014-02-13 03:43:41 -0500

    flake8 nazi

--- setup.py
+++ setup.py
@@ -4,16 +4,17 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'Use GitHub to serve your images.',
     'author': 'Jon Chen',
-    'url': 'https://github.com/fly/git-img',
-    'download_url': 'https://github.com/fly/git-img/archive/0.0.1.tar.gz',
     'author_email': 'dabestmayne@burrito.sh',
-    'version': '0.0.1',
+    'description': 'Use GitHub to serve your images.',
+    'download_url': 'https://github.com/fly/git-img/archive/0.0.1.tar.gz',
     'install_requires': ['gitpython'],
+    'long_description': open('README.md', 'rt').read(),
+    'name': 'gitimg',
     'packages': ['gitimg'],
     'scripts': ['gitimg'],
-    'name': 'gitimg'
+    'url': 'https://github.com/fly/git-img',
+    'version': '0.0.1',
 }
 
 setup(**config)
